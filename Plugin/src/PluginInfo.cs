f4fdf34b056e50247a1c5cde31653d491a98cd75
MrBeam <92742939+Mrbeam1235@users.noreply.github.com>
2025-04-09 17:55:05 -0600

    Add files via upload

--- Plugin/src/PluginInfo.cs
+++ Plugin/src/PluginInfo.cs
@@ -3,7 +3,7 @@ namespace H3TVR
 	internal static class PluginInfo
 	{
 		internal const string NAME = "H3TVR";
-		internal const string GUID = "com.jeditobiwan.h3tvr";
-		internal const string VERSION = "1.0.0";
+        internal const string GUID = "com.MrBeam.h3tvr";
+        internal const string VERSION = "1.0.5";
 	}
 }
